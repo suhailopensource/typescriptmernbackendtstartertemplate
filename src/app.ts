9c58ab76a947d4c03a6452b1205ec10fada3d1e1
Suhail <suhailcollegemail@gmail.com>
2024-05-01 22:24:08 +0530

    complete

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,12 @@
 import express, { NextFunction, Request, Response } from "express";
+import { connectDb } from "./config/db.js";
+import formRoute from "./routes/form.js";
 
+connectDb();
 const app = express();
+
+app.use(express.json());
+app.use("/api/v1/form", formRoute);
 
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
   res.send("HELLO WORLD TYPESCrIPT IS WORKING");
